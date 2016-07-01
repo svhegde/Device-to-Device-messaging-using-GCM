62f7a79cab6b920454facdfce7c19dedceab168a
Supada Hegde <supadaac@gmail.com>
2016-07-01 18:08:27 -0400

    Device to device messaging using GCM.

---
+++ gcm_server_files/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "XXXXXXXXXX");
+define("DB_USER", "XXXXXXXXXX");
+define("DB_PASSWORD", "XXXXXXXXXX");
+define("DB_DATABASE", "XXXXXXXXXX");
+
+/*
+ * Google Cloud Messaging API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyA81yS1VSj1WZXY_TXXXXXXXXXX"); // Place your Google API Key
+                          
+?>
